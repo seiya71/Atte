aa36ee1338c77f20595f38969051a73600a814a6
root <root@DESKTOP-5D49AFF>
2024-11-18 22:36:57 +0900

    ログイン登録機能

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -1,18 +1,19 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/login.css') }}">
+<link rel="stylesheet" href="{{ asset('css/login.css') }}" />
 @endsection
 
 @section('content')
 <div class="login-logo">
     <h2 class="login-logo__text">ログイン</h2>
 </div>
-<form class="login-form">
-    <input class="login-form__input" type="email" placeholder="メールアドレス" />
-    <input class="login-form__input" type="password" placeholder="パスワード" />
+<form class="login-form" action="/login" method="post">
+    @csrf
+    <input class="login-form__input" type="email" name="email" placeholder="メールアドレス" />
+    <input class="login-form__input" type="password" name="password" placeholder="パスワード" />
     <button type="submit">ログイン</button>
 </form>
 <p class="login-form__text">アカウントをお持ちでない方はこちらから</p>
-<a class="login-form__register" href="#">会員登録</a>
+<a class="login-form__register" href="/register">会員登録</a>
 @endsection
